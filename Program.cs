ebb72ded87f569d53049da7011a4c1c48d2baf54
andreadldev <andrea.delisi2@gmail.com>
2023-03-24 16:45:03 +0100

    ciclo indirizzi

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿var path = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).FullName).FullName).FullName).FullName;
+﻿using csharp_lista_indirizzi.classes;
+
+var path = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).FullName).FullName).FullName).FullName;
 StreamReader file = File.OpenText(@$"{path}\data\addresses.csv");
+
+var addresses = new List<string>();
+//var addresses = new List<Address>();
 
 while (!file.EndOfStream)
 {
     string line = file.ReadLine();
-    Console.WriteLine(line);
+    //Console.WriteLine(line);
+    addresses.Add(line);
 }
-
 file.Close();
+
+for (int i = 1; i < addresses.Count; i++)
+{
+    if (i > 1)
+    {
+        Console.WriteLine();
+    }
+    string address = addresses[i];
+    string[] arr = address.Split(",");
+
+    for (int x = 0; x < arr.Length; x++)
+    {
+        Console.Write("\"" + arr[x]);
+        if (x != arr.Length - 1)
+        {
+            Console.Write("\", ");
+        }
+        else Console.Write("\"");
+    }
+}
---
+++ classes/Address.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi.classes
+{
+    public class Address
+    {
+        public string name;
+        public string surname;
+        public string street;
+        public string city;
+        public string province;
+        public string zip;
+
+        public Address(string name, string surname, string street, string city, string province, string zip)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.zip = zip;
+        } 
+    }
+}
