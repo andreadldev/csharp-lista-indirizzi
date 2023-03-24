6c50aeb690957e2672fd2a72385f97e7cdc8e092
andreadldev <andrea.delisi2@gmail.com>
2023-03-24 18:54:49 +0100

    Aggiunta lista oggetti istanziati, metodo per verificare i valori e stampa indirizzi

--- Program.cs
+++ Program.cs
@@ -1,35 +1,57 @@
 ﻿using csharp_lista_indirizzi.classes;
+using System.Net;
 
 var path = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).FullName).FullName).FullName).FullName;
 StreamReader file = File.OpenText(@$"{path}\data\addresses.csv");
 
-var addresses = new List<string>();
-//var addresses = new List<Address>();
+var addresses = new List<Address>();
 
 while (!file.EndOfStream)
 {
-    string line = file.ReadLine();
-    //Console.WriteLine(line);
-    addresses.Add(line);
+    string? line = file.ReadLine();
+    var element = line.Split(',');
+
+    if (element.Length == 6)
+    {
+        string? name = element[0];
+        string? surname = element[1];
+        string? street = element[2];
+        string? city = element[3];
+        string? province = element[4];
+        string? zip = element[5];
+
+        var address = new Address(CheckString(name), CheckString(surname), CheckString(street), CheckString(city), CheckString(province), CheckString(zip));
+        addresses.Add(address);
+    } 
+    else
+    {
+        continue;
+    }
 }
 file.Close();
 
 for (int i = 1; i < addresses.Count; i++)
 {
-    if (i > 1)
-    {
-        Console.WriteLine();
-    }
-    string address = addresses[i];
-    string[] arr = address.Split(",");
+    Console.WriteLine(addresses[i].name);
+    Console.WriteLine(addresses[i].surname);
+    Console.WriteLine(addresses[i].street);
+    Console.WriteLine(addresses[i].city);
+    Console.WriteLine(addresses[i].province);
+    Console.WriteLine(addresses[i].zip);
+    Console.WriteLine("===============");
+}
 
-    for (int x = 0; x < arr.Length; x++)
+string CheckString(string str)
+{
+    if (str == "")
     {
-        Console.Write("\"" + arr[x]);
-        if (x != arr.Length - 1)
-        {
-            Console.Write("\", ");
-        }
-        else Console.Write("\"");
+        return "NULL";
     }
+
+    else if (str.Substring(0, 1) == " ")
+    {
+        return str.Substring(1, str.Length - 1);
+    }
+
+    else return str;
 }
